e064f76a1cc6e7e5f7233f099a22cd635443c91f
- The Aarnold - <mane.imation9@gmail.com>
2025-04-23 22:18:59 +0200

    Debug stick can now toggle AI from entities

--- src/main/java/io/gadjalin/holywrench/HolywrenchMod.java
+++ src/main/java/io/gadjalin/holywrench/HolywrenchMod.java
@@ -2,10 +2,14 @@ package io.gadjalin.holywrench;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
@@ -26,14 +30,28 @@ public class HolywrenchMod {
 
     public HolywrenchMod(IEventBus modBus) {
         modBus.addListener(HolywrenchMod::onBuildCreativeModeTabContents);
-        //NeoForge.EVENT_BUS.addListener(HolywrenchMod::onBlockLeftClick);
-        //NeoForge.EVENT_BUS.addListener(HolywrenchMod::onBlockRightClick);
+        NeoForge.EVENT_BUS.addListener(HolywrenchMod::onEntityRightClick);
     }
 
     // Add debug stick to creative inventory
     private static void onBuildCreativeModeTabContents(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES)) {
             event.accept(Items.DEBUG_STICK);
+        }
+    }
+
+    private static void onEntityRightClick(PlayerInteractEvent.EntityInteract event) {
+        Player player = event.getEntity();
+        Entity target = event.getTarget();
+
+        if (!player.level().isClientSide && event.getItemStack().is(Items.DEBUG_STICK) && (target instanceof LivingEntity) && !(target instanceof Player)) {
+            event.setCanceled(true);
+            CompoundTag tag = new CompoundTag();
+            target.saveWithoutId(tag);
+            boolean hasAI = tag.getBoolean("NoAI");
+            tag.putBoolean("NoAI", !hasAI);
+            target.load(tag);
+            player.displayClientMessage(Component.literal(hasAI ? "IA activée" : "IA désactivée"), true);
         }
     }
 }
